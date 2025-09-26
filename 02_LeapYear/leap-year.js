c0311aeea04dc75604434b4a70f4f7bdb7a2f9fd
Dell <robert.maksymow@tutanota.com>
2025-09-26 18:17:14 +0100

    Done

--- 02_LeapYear/leap-year.js
+++ 02_LeapYear/leap-year.js
@@ -1,11 +1,29 @@
+
 export function leapYear(year){
-    if(year){
-        console.log('Year is a leap year');
-    } else {
-        console.log('Year is not a leap year');
+    if(year % 4 !== 0){
+        console.log("not by 4", year);
+        return false;
+    } 
+
+    if(year % 100 === 0 && year % 400 !== 0){
+        console.log("not by 400", year);
+        return false;
     }
+    
+    if(year % 400 === 0){
+        console.log("div by 400", year);
+        return true;
+    } 
 
-
+    if(year % 4 === 0){
+        console.log("div by 4", year);
+        return true;
+    } 
 }
 
-leapYear();
+// console.log(leapYear(1600)); // true
+// console.log(leapYear(1800)); // false
+// console.log(leapYear(1900)); // false
+// console.log(leapYear(1996)); // true
+// console.log(leapYear(1997)); // false
+// console.log(leapYear(2000)); // true
--- 02_LeapYear/leap-year.test.js
+++ 02_LeapYear/leap-year.test.js
@@ -5,13 +5,22 @@ describe('Leap Year', () => {
         expect(leapYear).toBeDefined();
     });
     
-    test.todo("A year is not a leap year if not divisible by 4"
-    );
+    test("A year is not a leap year if not divisible by 4", () => {
+        expect(leapYear(1997)).toBe(false);
+        expect(leapYear(2025)).toBe(false);
+        expect(leapYear(2000)).toBe(true);
+    });
 
-    test.todo("A year is a leap year if divisible by 4"
-    );
+    test("A year is a leap year if divisible by 4", () => {
+        expect(leapYear(1996)).toBe(true);
+    });
 
-    test.todo("A year is a leap year if divisible by 400");
+    test("A year is a leap year if divisible by 400", () => {
+        expect(leapYear(1800)).toBe(false);
+        expect(leapYear(2000)).toBe(true);
+    });
 
-    test.todo("A year is not a leap year if divisible by 100 but not by 400")
+    test("A year is not a leap year if divisible by 100 but not by 400", () => {
+        expect(leapYear(1900)).toBe(false);
+    })
 });
