975f65b4755e3f744e4dc216a083001843547b7b
Dell <robert.maksymow@tutanota.com>
2025-12-23 22:26:47 +0000

    Step two done. Arbitrary number size

--- 03_string-calculator.js
+++ 03_string-calculator.js
@@ -1,10 +1,11 @@
 export function stringCalculator(input) {
     inputAsNumbers = input.split(',').map(Number);
-    console.log(typeof inputAsNumbers[0]);
     console.log(inputAsNumbers);
     
     if (input === "") return 0;
-    if (inputAsNumbers.length === 1) return inputAsNumbers[0];
-    if (inputAsNumbers.length === 2) return inputAsNumbers[0] + inputAsNumbers[1];
+    if (inputAsNumbers.length > 0){
+        return inputAsNumbers.reduce((a, b) => a + b, 0);
+    }
+
 
 }
--- 03_string-calculator.test.js
+++ 03_string-calculator.test.js
@@ -13,3 +13,10 @@ test('If two numbers are provided, it returns their sum', () => {
     expect(stringCalculator("1,2")).toBe(3);
     expect(stringCalculator("10,20")).toBe(30);
 });
+
+test('If multiple numbers are provided, it returns their sum', () => {
+    expect(stringCalculator("1")).toBe(1);
+    expect(stringCalculator("5,1")).toBe(6);
+    expect(stringCalculator("10,20,30,40")).toBe(100);
+    expect(stringCalculator("1,2,3,4,5,6,7,8,9")).toBe(45);
+});
