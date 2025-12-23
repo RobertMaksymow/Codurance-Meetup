c4fe5c65ffa093483ce76879ba8a7ef4f5bf95d2
Dell <robert.maksymow@tutanota.com>
2025-12-23 21:28:36 +0000

    unfinished rubbish removed

--- api-crud-testing/getProducts.js
+++
@@ -1,7 +0,0 @@
-export const getProducts = async (req, res) => {
-  try {
-    res.status(200).json({ success: true, data: {apple: 5, bananas: 4} });
-  } catch (error) {
-    console.log("Error in fetching products", error.message);
-  }
-};
